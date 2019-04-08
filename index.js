48932624576d061c7578c8d1c2a15586a9241c05
Muhammad Riza <riza.riza69@gmail.com>
2019-04-08 11:43:33 +0700

    project Javascript Shape Console

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// The formula of rectangle area is
+// Area = width * length
+const rectangleWidth = 100
+const rectangleLength = 20
+
+const rectangleArea = rectangleWidth * rectangleLength
+
+console.log(rectangleArea);
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`)
+
+// Feel free to have some experiments with variety of shape formulas
